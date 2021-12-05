yawnoc/guitar

commit 320f57b4d8125374b89b481d6570c6f1b7c3c5fb

Fix missing \once (Fantasia 7, Bar 17)

dowland-fantasia-7/dowland-fantasia-7.ly
@@ -630,8 +630,7 @@ lowerMiddleVoiceMusic = {
       \overrideHorizontalShift #0 e4 a |
       \revertHorizontalShift
     \shiftOnn
-    \overrideHorizontalShift #0
-      gis2
+      \once \overrideHorizontalShift #0 gis2
     \shiftOn
       fis4. fis8 |
     \once \overrideHorizontalShift #0 e4

commit 1305ac3068395861dcdcef338ba7de43dd140cf7

Fix bad shift caused by 320f57b

dowland-fantasia-7/dowland-fantasia-7.ly
@@ -691,7 +691,10 @@ lowerMiddleVoiceMusic = {
     s2 s8 fis4. |
     s2 \once \overrideHorizontalShift #0 fis4. fis8 |
     e8 cis4. d4. d8 |
-    \once \overrideHorizontalShift #-1.3 cis4 cis <cis fis,>2 |
+    \once \overrideHorizontalShift #-1.3 cis4
+    \overrideHorizontalShift #0
+    cis <cis fis,>2 |
+    \revertHorizontalShift
     r8 b b b gis8. a16 b4 |
   }
   