yawnoc/guitar

commit d80d50585c48463b495024dc625f398bc26412b7

Mark open strings in partial barre (Fantasia 7, Bar 10)

dowland-fantasia-7/dowland-fantasia-7.ly
@@ -410,7 +410,7 @@ upperMiddleVoiceMusic = {
     \once \overrideFingeringPadding #1
     <gis-1>8 <a-3> <b-0> <cis-1>^\barre ½CII
       \barreSpan CII { <dis-4>8 cis16 \shiftOff dis } <e~ b_~>4 |
-    <e b> <dis-4> e
+    <e-0 b-0> <dis-4> e
     \shiftOn
   }
   

commit 653773b4dc6b845839c7c8248136be6f1a11e8b1

Fix fingering/tie collision caused by d80d505

conway.ily
@@ -107,6 +107,13 @@ overrideHorizontalShift = #(define-scheme-function
 )
 revertHorizontalShift = { \revert NoteColumn.force-hshift }
 
+overrideFingeringExtraOffset = #(define-scheme-function
+  (parser location offsetPair) (pair?)
+  #{
+    \override Fingering.extra-offset = #offsetPair
+  #}
+)
+
 overrideFingeringPadding = #(define-scheme-function
   (parser location distance) (number?)
   #{

dowland-fantasia-7/dowland-fantasia-7.ly
@@ -655,6 +655,7 @@ lowerMiddleVoiceMusic = {
     <b-2> <e-1> a <gis-1> |
       \revertHorizontalShift
     \stemUp
+    \once \overrideFingeringExtraOffset #'(0 . -0.5)
     \barreSpan ⅓CII { <fis-3>8 e }
       \overrideHorizontalShift #0 <fis-3>4 <e-1>
       \revertHorizontalShift